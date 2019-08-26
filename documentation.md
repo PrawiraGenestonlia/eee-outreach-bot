be7cf0777e0f2fa7917a674821aabcc568f986e6
PrawiraGenestonlia <prawira.96@gmail.com>
2019-08-26 21:54:35 +0800

    .

---
+++ documentation.md
@@ -0,0 +1,17 @@
+# Documentation
+
+## Table of Contents
+
+## Postman test
+
+```json
+{
+    "ok": true,
+    "result": {
+        "id": 580498412,
+        "is_bot": true,
+        "first_name": "Eee-outreach",
+        "username": "EEE_Outreach_bot"
+    }
+}
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+const Telegraf = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_API)
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('modern', ({ reply }) => reply('Yo'))
+bot.command('hipster', Telegraf.reply('λ'))
+bot.launch()
